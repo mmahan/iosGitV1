cd8f839dc4e2a664f91799bdfeb0a0dc94de2ff4
Matt Mahan <matthewrmahan@gmail.com>
2014-08-31 13:07:49 -0400

    added a test class

---
+++ MMTest.h
@@ -0,0 +1,13 @@
+//
+//  MMTest.h
+//  gitTest
+//
+//  Created by Matt Mahan on 8/31/14.
+//  Copyright (c) 2014 mmahan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMTest : NSObject
+
+@end
---
+++ MMTest.m
@@ -0,0 +1,13 @@
+//
+//  MMTest.m
+//  gitTest
+//
+//  Created by Matt Mahan on 8/31/14.
+//  Copyright (c) 2014 mmahan. All rights reserved.
+//
+
+#import "MMTest.h"
+
+@implementation MMTest
+
+@end
